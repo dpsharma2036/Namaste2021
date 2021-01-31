3ee500a03fa7ab2bd85772255f2f7c714cca711c
dpsha@DESKTOP-EJNNPU <dpsharma2036@gmail.com>
2021-01-31 00:13:25 -0500

    initial commit

---
+++ src/components/namaste/Namaste.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Namaste(props){
+    return(
+     <div className="Namaste">
+        Namaste, {props.name}!
+     </div>
+    );
+}
+
+export default Namaste;
